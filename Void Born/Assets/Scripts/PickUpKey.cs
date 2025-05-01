903338ca31dc09dedc003442575f8e5e14def97e
Kexnoo <jbtaylor1@student.fullsail.edu>
2025-05-01 16:41:43 -0500

    fixed a few more collider on first floor and switched keycollection to raycast

--- Void Born/Assets/Scripts/PickUpKey.cs
+++ Void Born/Assets/Scripts/PickUpKey.cs
@@ -1,63 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class PickUpKey : MonoBehaviour
 {
-    public GameObject keyOB;
-    public GameObject invOB;
-    public GameObject pickUpText;
-    public AudioSource keySound;
+    public GameObject keyOB;          // The key object in the scene
+    public GameObject invOB;          // The key's icon in the inventory
+    public GameObject pickUpText;     // UI text shown when looking at the key
+    public AudioSource keySound;      // Sound that plays when key is picked up
 
-    public bool inReach;
+    public float interactDistance = 3f; // How far the player can interact from
 
+    private bool keyPickedUp = false;
 
     void Start()
     {
-        inReach = false;
         pickUpText.SetActive(false);
         invOB.SetActive(false);
     }
 
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "Reach")
-        {
-            inReach = true;
-            pickUpText.SetActive(true);
-
-        }
-
-        Debug.Log("[PickUpKey] Player in reach!");
-
-    }
-
-    void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.tag == "Reach")
-        {
-            inReach = false;
-            pickUpText.SetActive(false);
-
-        }
-    }
-
-
     void Update()
     {
-        if (inReach && Input.GetButtonDown("Interact"))
+        if (keyPickedUp) return;
+
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+        RaycastHit hit;
+
+        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * 6f, Color.blue);
+        if (Physics.Raycast(ray, out hit, interactDistance))
         {
-            keyOB.SetActive(false);
-            keySound.Play();
-            invOB.SetActive(true);
+            if (hit.collider.gameObject == keyOB)
+            {
+                pickUpText.SetActive(true);
+
+                if (Input.GetButtonDown("Interact"))
+                {
+                    keyOB.SetActive(false);
+                    keySound.Play();
+                    invOB.SetActive(true);
+                    pickUpText.SetActive(false);
+                    keyPickedUp = true;
+
+                    Debug.Log("[PickUpKey] Key picked up!");
+                }
+            }
+            else
+            {
+                pickUpText.SetActive(false);
+            }
+        }
+        else
+        {
             pickUpText.SetActive(false);
         }
-        if (Input.GetButtonDown("Interact"))
-        {
-            Debug.Log("[PickUpKey] Interact button pressed!");
-        }
-
-
     }
 }
